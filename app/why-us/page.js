4020af2ae4c1df8bdf7303d03cd4b91baa99bcd3
Mustafa <mustafaprogrammer786@gmail.com>
2025-11-04 05:38:42 -0800

    URL changes fixed

--- app/why-us/page.js
+++ app/why-us/page.js
@@ -74,7 +74,7 @@ export default function WhyUs() {
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: 'url("/images/whyusbg.jpg")'
+            backgroundImage: 'url("/images/whyusBg.jpg")'
           }}
         />
         {/* Green & Gold Overlay */}
